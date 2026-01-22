c043d38ca12f732fa42e6cc3de10a07ce1f5bb27
kame77770718 <kame7774@gmail.com>
2026-01-22 12:44:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("JavaScript準備OK");
+const subjectInput = document.getElementById("subject");
+const timeInput = document.getElementById("time");
+const addButton = document.getElementById("add-btn");
+const list = document.getElementById("list");
+
+addButton.addEventListener("click", function () {
+  console.log("ボタンが押されました");
+});
+
+addButton.addEventListener("click", function () {
+  const subject = subjectInput.value;
+  const time = timeInput.value;
+
+  console.log(subject, time);
+});
+
+addButton.addEventListener("click", function () {
+  const subject = subjectInput.value;
+  const time = timeInput.value;
+
+  const li = document.createElement("li");
+  li.textContent = subject + "：" + time + "時間";
+
+  list.appendChild(li);
+});
