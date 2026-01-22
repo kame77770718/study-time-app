e9da1fe5367e5876d0a2d035e4ef568c4477fc41
kame77770718 <kame7774@gmail.com>
2026-01-22 13:11:03 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,31 +3,7 @@ const timeInput = document.getElementById("time");
 const addButton = document.getElementById("add-btn");
 const list = document.getElementById("list");
 
-addButton.addEventListener("click", function () {
-  const subject = subjectInput.value;
-  const time = timeInput.value;
-
-  if (subject.trim() === "" || time <= 0) {
-  alert("正しい科目名と時間を入力してください");
-  return;
-}
-
-function addItem(subject, time) {
-  const li = document.createElement("li");
-  li.textContent = subject + "：" + time + "時間 ";
-
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "削除";
-
-  deleteBtn.addEventListener("click", function () {
-    li.remove();
-    saveData();
-  });
-
-  li.appendChild(deleteBtn);
-  list.appendChild(li);
-}
-
+// 追加処理
 addButton.addEventListener("click", function () {
   const subject = subjectInput.value;
   const time = timeInput.value;
@@ -44,12 +20,29 @@ addButton.addEventListener("click", function () {
   timeInput.value = "";
 });
 
-  
+// 1件追加する関数
+function addItem(subject, time) {
+  const li = document.createElement("li");
+  li.textContent = subject + "：" + time + "時間 ";
 
+  const deleteBtn = document.createElement("button");
+  deleteBtn.textContent = "削除";
+
+  deleteBtn.addEventListener("click", function () {
+    li.remove();
+    saveData();
+  });
+
+  li.appendChild(deleteBtn);
+  list.appendChild(li);
+}
+
+// 保存
 function saveData() {
   localStorage.setItem("studyData", list.innerHTML);
 }
 
+// 読み込み
 function loadData() {
   const data = localStorage.getItem("studyData");
   if (data) {
